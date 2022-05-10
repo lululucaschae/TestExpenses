82d1018263b4c15c1e0afbbd2c43346235e9fd25
Lucas Chae <hi@lucaschae.com>
2022-05-10 11:42:51 +0900

    Initial Commit

---
+++ TestExpenses/TestExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestExpensesApp.swift
+//  TestExpenses
+//
+//  Created by Lucas Chae on 5/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TestExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
